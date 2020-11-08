d3a7bc351961db84e228332e9883e2a9a4bc0fda
Letícia Cellurale <leticia.cellurale@gmail.com>
2020-11-08 17:23:59 -0300

    :bug: fix event card timestamp

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -1,10 +1,14 @@
 import './style.css';
 
-const EventCard = ({ title, startTime, endTime }) => {
+const EventCard = ({ description, startTime, endTime, id }) => {
+
+	const startHour = new Date(startTime*1000).getHours() + ':' + new Date(startTime*1000).getMinutes();
+	const endHour = new Date(endTime*1000).getHours() + ':' + new Date(endTime*1000).getMinutes();
+
 	return (
 		<li className="eventCard">
-			<h3 className="title">{title}</h3>
-			<p className="time">{startTime} até {endTime}</p>
+			<h3 className="title">{description}</h3>
+			<p className="time">{startHour} até {endHour}</p>
 		</li>
 	)
 }
